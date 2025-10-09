f38ece04740eb86e58f6891fede24799905df174
chitizawasthi <chitizawasthi@gmail.com>
2025-10-09 14:30:01 +0530

    implemented the arraylist concept

---
+++ src/listandarraylist/example1.java
@@ -0,0 +1,9 @@
+package listandarraylist;
+import java.util.ArrayList;
+public class example1
+{
+    public static void main(String[] args)
+    {
+        ArrayList <Integer> list=new ArrayList<>();
+    }
+}
